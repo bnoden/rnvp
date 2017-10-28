eca0e0368f78fd4ecaf023d0ad477716271c31ff
bnoden <brandroid369@gmail.com>
2017-10-27 21:21:55 -0500

    rm extraneousness

--- README.md
+++ README.md
@@ -2,4 +2,5 @@ React Native Video Project
 
 todo:  
 Include video title from metadata, rather than just the file name.  
+Deal with Android compatibility issues.  
 Much, much more.  
--- app/components/TextInput/InputWithButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Text, TextInput, TouchableHighlight, View } from 'react-native';
-
-import styles from './styles';
-
-const InputWithButton = () => null;
-
-export default InputWithButton;
